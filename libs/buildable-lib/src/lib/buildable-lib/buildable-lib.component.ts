2896d5140df08c5da6b59858eb25ad2691073810
Andrés Arias <arias9306@gmail.com>
2024-01-02 14:31:21 -0500

    feat: add buildable lib to test

---
+++ libs/buildable-lib/src/lib/buildable-lib/buildable-lib.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nx-generators-buildable-lib',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './buildable-lib.component.html',
+  styleUrl: './buildable-lib.component.scss',
+})
+export class BuildableLibComponent {}
